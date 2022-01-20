eee8ff8e0e04f99feac9ea430c3082bad5f9f1fa
michaeldouglasvg <douglas1999michael@gmail.com>
2022-01-20 16:06:00 +0300

    portifolio

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 function myInfor(){
     alert("Hi, Am Michael Douglas. Build this portifolio in basis of HTML5, CSS3, JS and Libraries");
 }
+
+// navigation section
+function showMenu(){
+
+    const menu = document.getElementById("toggle");
+
+    if(menu.style.display == "none"){
+
+        menu.style.display = "block";
+
+    }else{
+
+        menu.style.display = "none";
+    }
+}
