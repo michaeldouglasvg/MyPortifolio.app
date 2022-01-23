eb1c55376c2ff6f8f6bfee912dff141501ee2957
michaeldouglasvg <douglas1999michael@gmail.com>
2022-01-23 10:21:01 +0300

    portifolio

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// toggle dak mode
+function darkMode(){
+    alert("Dark mode functionality is currently under implimentation, Will be functioning soon, Contact for more information")
+}
+
+
 function myInfor(){
     alert("Hi, Am Michael Douglas. Build this portifolio in basis of HTML5, CSS3, JS and Libraries");
 }
@@ -51,3 +57,30 @@ function displayTime(){
 }
 
 setInterval(displayTime, 10);
+
+// home page js
+const homeMessage = document.getElementById("mymessage");
+homeMessage.textContent = "Hi, am a student at South Easter Kenya University. Pursuing Computer Science (CS), am optimized in technology. Providing optimum solutions to complex technical problems. Ready to learn and master from the solutions, and change in technology.";
+
+
+// skills page
+const skillspage = document.getElementById("skillpage");
+skillspage.textContent = "Learning is one of my favorite to provide possible solutions, Different languages I use provides different solutions for different problems. Rapid change in technology requires practice and much more to master step by step. Here are some of my skills learning and mastering them is what is do";
+
+// projects page js
+const projectPage = document.getElementById("project");
+console.log(projectPage.getElementsByTagName("p"));
+
+const btnShow = document.querySelectorAll(".language");
+const cardimage = document.querySelectorAll(".imagescards");
+const btnback = document.querySelectorAll(".backpress");
+const cardback = document.querySelectorAll(".cardback");
+
+btnShow.forEach(function(btnsingle){
+
+    btnsingle.addEventListener("click", function(e){
+        e.preventDefault();
+        alert("hello")
+    });
+
+});
